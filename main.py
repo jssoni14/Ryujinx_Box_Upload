1fd871d600f6f00a99fe54e28d5e9e51d87c7499
jssoni14 <jsoni14@gmail.com>
2024-03-26 16:20:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -30,27 +30,12 @@ base_url = 'https://github.com'
 grab = requests.get(urls)
 soup = BeautifulSoup(grab.text, 'html.parser')
 
-# opening a file in write mode
-f = open("Ryzu_links.txt", "w")
-# traverse paragraphs from soup
+
+# traverse links from soup
 for link in soup.find_all("a"):
    data = link.get('href')
    Ryujinx_URL.append(data)
-   f.write(data)
-   f.write("\n")
-print(Ryujinx_URL)
-f.close()
-"""
-with open("test1.txt", "r") as text_file:
-  for line in text_file:
-      if re.search(pattern_zip, line):
-          print(line)
-          url_links.append(line)
 
-          match_obj = re.search(pattern_headless, u)
-    if not match_obj:
-      print(u)
-"""
 for u in Ryujinx_URL:
    if re.search(pattern_zip, u):
     url_obj = re.search(pattern_headless, u)
